9e41c229d587f4ce0b4f61dcc44c10ce0f7321c6
Abeleshev <abeleshev1993@gmail.com>
2022-10-24 18:41:25 +0300

    add edit mode to service item page

--- src/pages/ServiceItem.tsx
+++ src/pages/ServiceItem.tsx
@@ -28,9 +28,22 @@ const ServiceItem=()=>{
     const navigator = useNavigate()
 
     useEffect(()=>{
-        const regularExpression = new RegExp('ab+c')
+        const config = {
+            headers: {
+              'Authorization': 'Bearer ' + userState.token
+            }
+          }
         if(servicesId !=='new'){
+            
             setRenderMode(componentMode.reading)
+            axios.get(`${BACKEND_URL}/services/getOne?id=${servicesId}`, config).then(resultRequest=>{
+                console.log(resultRequest.data)
+                if(resultRequest.status === 200){
+                    setName(resultRequest.data.data.name)
+                    setDescription(resultRequest.data.data.description)
+                    setSelectedImage(resultRequest.data.data.img)
+                }
+            })
         }
     }
     ,[servicesId])
@@ -62,6 +75,24 @@ const ServiceItem=()=>{
             })
     }
 
+    const saveChanges=()=>{
+        const sendingData = {name, description, img: selectedImage}
+        const config = {
+            headers: {
+              'Authorization': 'Bearer ' + userState.token
+            }
+          }
+        axios.post(`${BACKEND_URL}/services/update?id=${servicesId}`, sendingData, config).then(replyRequest =>{
+            if(replyRequest.status === 200){
+                navigator(-1)
+            }
+        }).catch(error=>{
+            console.log(error)
+                if(axios.isAxiosError(error)){
+                    alert(error.response?.data.message)
+                }
+            })
+    }
     const creatingMode = (
         <form className={styles.dataContainer}>
             <label htmlFor="name">Name:</label>
@@ -75,8 +106,23 @@ const ServiceItem=()=>{
             
             <Button title='Create new' onClick={createNew} />
         </form>)
+
+    const editMode = (
+        <form className={styles.dataContainer}>
+            <label htmlFor="name">Name:</label>
+            <input type="text" name="name" id="name" value={name} onChange={(event)=>setName(event.target.value)}/>
+
+            <label htmlFor="description">Description:</label>
+            <input type="text" name="description" id="description" value={description} onChange={(event)=>setDescription(event.target.value)} />
+            
+            <img className={styles.imgBox} src={selectedImage} alt="image" onClick={()=>setVisibleModalWindow(true)}/>
+            {visibleModalWindow&&<ModalWindow setVisible={setVisibleModalWindow}><ImgSelector searchString={name} setSelectedImg={setSelectedImage} /></ModalWindow>}
+            
+            <Button title='save changes' onClick={saveChanges} />
+        </form>        
+    )
     return(<>
-        {renderMode === componentMode.creating ?creatingMode:<h1>its page for see element</h1>}
+        {renderMode === componentMode.creating ?creatingMode:editMode}
     </>
     )
 }
